05a690d27cd4f1f7a2ee185f787a0d37c256cda1
Damyan24 <damqndraganov@gmail.com>
2024-01-18 22:12:15 +0200

    commit

---
+++ src/main/java/Ballshop/models/Item.java
@@ -0,0 +1,64 @@
+package Ballshop.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item" , schema="ballshop")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name ="item_name")
+    private String itemName;
+
+    @Column(name = "item_price")
+    private Double itemPrice;
+
+
+    @Column(name="user_id")
+    private int userId;
+
+    @Column(name = "image_identifier")
+    private String imageIdentifier;
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(Double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getImageIdentifier() {
+        return imageIdentifier;
+    }
+
+    public void setImageIdentifier(String imageIdentifier) {
+        this.imageIdentifier = imageIdentifier;
+    }
+}
