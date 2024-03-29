f90db3d133bec8f857cace61a46ff6088c189785
Damyan24 <damqndraganov@gmail.com>
2024-03-29 22:30:55 +0200

    commit

---
+++ src/main/resources/static/js/myscript.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.add-to-basket');
+
+// Loop through each button and attach an event listener
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        // Retrieve the ID of the clicked button
+        const buttonId = this.id;
+        
+    });
+});
